937559836d2265bbf37219b2562870f3f74c75f1
AJ <alexanderjessediaz@gmail.com>
2020-09-15 13:38:55 -0600

    remove default css from App.css import Container Row Col and Media to App.js. id video-container to App.js container.add justify content: center  in App.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Media from 'react-bootstrap/Media';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
 
 
 
@@ -9,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="#home">Random Vidz</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
       <Nav 
@@ -20,14 +26,33 @@ function App() {
           <Nav.Link href="/home">Active</Nav.Link>
         </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-1">Link</Nav.Link>
+            <Nav.Link eventKey="link-1">Category 1</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-2">Link</Nav.Link>
+            <Nav.Link eventKey="link-2">Category 2</Nav.Link>
           </Nav.Item>
       </Nav>
       </Navbar.Collapse>
   </Navbar>
+
+  <Container fluid id="video-container">
+  <Row>
+  <Col > 
+  <Media >
+  <img
+    width={64}
+    height={64}
+    className="mr-3"
+    src="holder.js/64x64"
+    alt="Generic placeholder"
+  />
+  <Media.Body>
+    
+  </Media.Body>
+</Media>
+</Col>
+</Row>
+</Container>
     </div>
   );
 }
